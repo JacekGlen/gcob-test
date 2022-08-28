94fda52ceebe1479b2930e6ab3b02377845d415d
sami.beyoglu <sami.beyoglu@linkit.nl>
2022-08-28 23:48:56 +0200

    Implement CustomerController.Get

--- Rabobank.TechnicalTest.GCOB/Controllers/CustomerController.cs
+++ Rabobank.TechnicalTest.GCOB/Controllers/CustomerController.cs
@@ -3,8 +3,11 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Rabobank.TechnicalTest.GCOB.Dtos;
+using Rabobank.TechnicalTest.GCOB.Enums;
+using Rabobank.TechnicalTest.GCOB.Services;
 
 namespace Rabobank.TechnicalTest.GCOB.Controllers
 {
@@ -13,16 +16,26 @@ namespace Rabobank.TechnicalTest.GCOB.Controllers
     public class CustomerController : ControllerBase
     {
         private readonly ILogger<CustomerController> _logger;
+        private readonly ICustomerService _customerService;
 
-        public CustomerController(ILogger<CustomerController> logger)
+        public CustomerController(ILogger<CustomerController> logger, ICustomerService customerService)
         {
             _logger = logger;
+            _customerService = customerService;
         }
 
         [HttpGet]
-        public Task<CustomerDto> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            throw new NotImplementedException();
+            var result = await _customerService.GetCustomer(id);
+            
+            if(result.Succeeded)
+                return Ok(result.Value);
+            
+            if(result.ErrorType == ErrorType.ValidationError)
+                return BadRequest(result.ErrorMessage);
+            
+            return StatusCode((int) HttpStatusCode.InternalServerError);
         }
 
         [HttpPost]
