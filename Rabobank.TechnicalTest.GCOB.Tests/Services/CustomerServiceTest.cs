a5dc7fd4d2e3d4ee1132b183d3940bb1b3c5cc9a
sami.beyoglu <sami.beyoglu@linkit.nl>
2022-08-28 14:19:34 +0200

    Add CustomerService.AddCustomer unit test
    that tests if the customer already exists in the DB

--- Rabobank.TechnicalTest.GCOB.Tests/Services/CustomerServiceTest.cs
+++ Rabobank.TechnicalTest.GCOB.Tests/Services/CustomerServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
@@ -6,6 +7,7 @@ using Castle.Core.Logging;
 using FluentAssertions;
 using Moq;
 using Rabobank.TechnicalTest.GCOB.Dtos;
+using Rabobank.TechnicalTest.GCOB.Enums;
 using Rabobank.TechnicalTest.GCOB.Repositories;
 using Rabobank.TechnicalTest.GCOB.Services;
 
@@ -46,6 +48,27 @@ namespace Rabobank.TechnicalTest.GCOB.Tests.Services
             // Assert
             serviceResult.Succeeded.Should().BeTrue();
             serviceResult.Value.Should().BeEquivalentTo(customerToBeAdded);
+        }
+
+        [TestMethod]
+        public async Task AddCustomer_GivenTheCustomerAlreadyExists_WhenICallAddCustomer_ThenServiceReturnsErrorMessage()
+        {
+            // Arrange
+            var customerToBeAdded = _fixture.Create<CustomerDto>();
+            var errorMessage = $"Cannot insert customer with identity '{customerToBeAdded.Id}' as it already exists in the collection";
+            var mockRepo = _fixture.Freeze<Mock<ICustomerRepository>>();
+            mockRepo.Setup(x => x.InsertAsync(customerToBeAdded)).Throws(new Exception(errorMessage));
+            mockRepo.Setup(x => x.GetAsync(customerToBeAdded.Id)).Returns(Task.FromResult(customerToBeAdded));
+            var sut = _fixture.Create<CustomerService>();
+
+            // Act
+            var serviceResult = await sut.AddCustomer(customerToBeAdded);
+
+            // Assert
+            serviceResult.Succeeded.Should().BeFalse();
+            serviceResult.Value.Should().BeNull();
+            serviceResult.ErrorType.Should().Be(ErrorType.Exception);
+            serviceResult.ErrorMessage.Should().Be(errorMessage);
         }
     }
 }
